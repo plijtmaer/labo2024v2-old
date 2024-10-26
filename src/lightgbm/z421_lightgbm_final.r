d2c9ad1f60cf781795206634c2c03c6c8bcd89bb
plijtmaer <plijtmaer@gmail.com>
2024-10-26 15:37:49 +0000

    auto commit 29       20241026 153741

--- src/lightgbm/z421_lightgbm_final.r
+++ src/lightgbm/z421_lightgbm_final.r
@@ -164,7 +164,7 @@ setorder(tb_entrega, -prob)
 # genero archivos con los  "envios" mejores
 # suba TODOS los archivos a Kaggle
 
-cortes <- seq(750, 1200, by = 150)
+cortes <- seq(800, 2000, by = 100)
 for (envios in cortes) {
   tb_entrega[, Predicted := 0L]
   tb_entrega[1:envios, Predicted := 1L]
